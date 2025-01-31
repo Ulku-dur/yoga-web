c665a08a97c75d7602ce3f3b22144c37beb794b3
Ulku Koker <h.ulkukoker@gmail.com>
2025-01-31 22:26:20 +0300

    first commit

---
+++ src/layouts/PriceCard.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types"; // prop-types kütüphanesini import ediyoruz
+import { TiTick } from "react-icons/ti";
+
+const PriceCard = ({ price, plan }) => {
+  return (
+    <div className="w-full md:w-4/5 p-3 border-2 border-white bg-black space-y-4 text-center cursor-pointer hover:scale-95 transition duration-200 ease-in-out">
+      <p className="text-2xl font-semibold py-2">{plan}</p>
+      <h className="text-5xl font-bold">${price}</h>
+      <h3 className="text-xl font-semibold">/month</h3>
+
+      <div className="space-y-2">
+        <div className="flex">
+          <TiTick size={42} />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            inventore!
+          </p>
+        </div>
+        <div className="flex">
+          <TiTick size={42} />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            inventore!
+          </p>
+        </div>
+        <div className="flex pb-6">
+          <TiTick size={42} />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            inventore!
+          </p>
+        </div>
+
+        <button className="bg-white py-1 px-5 text-black text-lg font-bold rounded-md hover:bg-purple-300 transition ease-linear">
+          Book Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+// Prop-types tanımlamaları
+PriceCard.propTypes = {
+  price: PropTypes.number.isRequired, // price bir number olmalı ve zorunlu
+  plan: PropTypes.string.isRequired, // plan bir string olmalı ve zorunlu
+};
+
+export default PriceCard;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#29294a",
+        secondary: "#6B21A8",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "3rem",
+          sm: "1rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
